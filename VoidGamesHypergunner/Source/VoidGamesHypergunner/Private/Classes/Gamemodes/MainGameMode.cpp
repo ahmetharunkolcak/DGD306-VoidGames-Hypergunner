74ff66ac03b52058e33427a76568fe43815444ba
Ali Doğan Kurşun <alidogankursun2362@gmail.com>
2025-06-01 00:12:14 +0300

    convert prototyped basic multiplayer spawn system to C++ code

--- VoidGamesHypergunner/Source/VoidGamesHypergunner/Private/Classes/Gamemodes/MainGameMode.cpp
+++ VoidGamesHypergunner/Source/VoidGamesHypergunner/Private/Classes/Gamemodes/MainGameMode.cpp
@@ -1,1 +1,52 @@
 ﻿#include "VoidGamesHypergunner/Public/Classes/Gamemodes/MainGameMode.h"
+
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+void AMainGameMode::BeginPlay() {
+	Super::BeginPlay();
+
+	if (UWorld* CurrentWorld = GetWorld()) {
+		TArray<AActor*> PlayerStarts;
+		UGameplayStatics::GetAllActorsOfClass(CurrentWorld, APlayerStart::StaticClass(), PlayerStarts);
+		for (int32 CurrentIndex = 0; CurrentIndex < PlayerStarts.Num(); ++CurrentIndex) {
+			const AActor* CurrentPlayerStart = PlayerStarts[CurrentIndex];
+			FTransform CurrentPlayerStartTransform = CurrentPlayerStart -> GetTransform();
+			
+			TSubclassOf<APlayerCharacter> SpawningActorClass = nullptr;
+			if (this -> PlayerCharacters.IsValidIndex(CurrentIndex)) {
+				SpawningActorClass = this -> PlayerCharacters[CurrentIndex];
+			}
+
+			if (SpawningActorClass == nullptr) {
+				UE_LOG(LogTemp,
+				       Warning,
+				       TEXT("AMainGameMode::BeginPlay: Spawning Actor with Index %d could not found: Class is null"),
+				       CurrentIndex);
+
+				continue;
+			}
+
+			if (CurrentIndex == 0) {
+				AActor* SpawnedPlayerActor = CurrentWorld -> SpawnActor(SpawningActorClass, &CurrentPlayerStartTransform);
+				APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+				APawn* PlayerPawn = Cast<APawn>(SpawnedPlayerActor);
+				PlayerController -> Possess(PlayerPawn);
+			} else {
+				UGameInstance* CurrentGameInstance = GetGameInstance();
+
+				FString OutErrorMessage;
+				if (const ULocalPlayer* CreatedLocalPlayer = CurrentGameInstance -> CreateLocalPlayer(-1, OutErrorMessage, true)) {
+					AActor* SpawnedPlayerActor = CurrentWorld -> SpawnActor(SpawningActorClass, &CurrentPlayerStartTransform);
+					APlayerController* PlayerController = CreatedLocalPlayer -> GetPlayerController(CurrentWorld);
+					APawn* PlayerPawn = Cast<APawn>(SpawnedPlayerActor);
+					PlayerController -> Possess(PlayerPawn);
+				} else {
+					UE_LOG(LogTemp,
+					       Warning,
+					       TEXT("AMainGameMode::BeginPlay - Failed to create local player: %s"), *OutErrorMessage);
+				}
+			}
+		}
+	}
+}
--- VoidGamesHypergunner/Source/VoidGamesHypergunner/Public/Classes/Gamemodes/MainGameMode.h
+++ VoidGamesHypergunner/Source/VoidGamesHypergunner/Public/Classes/Gamemodes/MainGameMode.h
@@ -2,9 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+
+#include "Classes/Characters/PlayerCharacter.h"
+
 #include "MainGameMode.generated.h"
 
 UCLASS()
 class VOIDGAMESHYPERGUNNER_API AMainGameMode : public AGameModeBase {
 	GENERATED_BODY()
+
+	protected:
+		virtual void BeginPlay() override;
+
+	protected:
+		UPROPERTY(EditAnywhere, Category = "Custom | Player Selection")
+		TArray<TSubclassOf<APlayerCharacter>> PlayerCharacters = {};
 };
