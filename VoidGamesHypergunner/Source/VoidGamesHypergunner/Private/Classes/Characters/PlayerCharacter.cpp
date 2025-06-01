4e83821ed2f7fb85f90c169bf038dab2dcd54c9d
Ali Doğan Kurşun <alidogankursun2362@gmail.com>
2025-06-02 01:13:02 +0300

    convert prototyped movement input to C++ code

--- VoidGamesHypergunner/Source/VoidGamesHypergunner/Private/Classes/Characters/PlayerCharacter.cpp
+++ VoidGamesHypergunner/Source/VoidGamesHypergunner/Private/Classes/Characters/PlayerCharacter.cpp
@@ -1,5 +1,41 @@
 ﻿#include "VoidGamesHypergunner/Public/Classes/Characters/PlayerCharacter.h"
 
+#include "GameFramework/PlayerController.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
 APlayerCharacter::APlayerCharacter() {
 	PrimaryActorTick.bCanEverTick = false;
+}
+
+void APlayerCharacter::PossessedBy(AController* NewController) {
+	Super::PossessedBy(NewController);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(NewController)) {
+		if (ULocalPlayer* LocalPlayer = PlayerController -> GetLocalPlayer()) {
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer)) {
+				Subsystem -> AddMappingContext(this->InputMappingContext, 0);
+			}
+		}
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value) {
+	float MovementScaleValue = Value.Get<float>();
+
+	FRotator Rotation = GetControlRotation();
+	FRotator RotationToGetRight = FRotator(Rotation.Roll, 0.0f, Rotation.Yaw);
+	FVector WorldActorRightDirection = UKismetMathLibrary::GetRightVector(RotationToGetRight);
+
+	AddMovementInput(WorldActorRightDirection, MovementScaleValue);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
+		EnhancedInputComponent -> BindAction(this -> MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+	}
 }
--- VoidGamesHypergunner/Source/VoidGamesHypergunner/Public/Classes/Characters/PlayerCharacter.h
+++ VoidGamesHypergunner/Source/VoidGamesHypergunner/Public/Classes/Characters/PlayerCharacter.h
@@ -2,7 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "InputActionValue.h"
+
 #include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class VOIDGAMESHYPERGUNNER_API APlayerCharacter : public ACharacter {
@@ -10,4 +16,18 @@ class VOIDGAMESHYPERGUNNER_API APlayerCharacter : public ACharacter {
 
 	public:
 		APlayerCharacter();
+
+	protected:
+		virtual void PossessedBy(AController* NewController) override;
+
+		void Move(const FInputActionValue& Value);
+
+		virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	protected:
+		UPROPERTY(EditAnywhere, Category = "Custom|Input")
+		UInputMappingContext* InputMappingContext = nullptr;
+
+		UPROPERTY(EditAnywhere, Category = "Custom|Input|Actions")
+		UInputAction* MoveAction = nullptr;
 };
